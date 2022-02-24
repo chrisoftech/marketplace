06758dcae325941dad56556043b92acf8f398bb8
Christian Onwe <chrisoftech@gmail.com>
2022-02-24 16:09:40 +0000

    Implemented create-product usecase.

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -2,8 +2,49 @@ pragma solidity >=0.5.0;
 
 contract Marketplace {
     string public name;
+    uint256 public productCount = 0;
+    mapping(uint256 => Product) public products;
+
+    struct Product {
+        uint256 id;
+        string name;
+        uint256 price;
+        address owner;
+        bool purchased;
+    }
+
+    event ProductCreated(
+        uint256 id,
+        string name,
+        uint256 price,
+        address owner,
+        bool purchased
+    );
 
     constructor() public {
         name = "Dapp University Marketplace";
+    }
+
+    function createProduct(string memory _name, uint256 _price) public {
+        // require valid name
+        require(bytes(_name).length > 0);
+
+        // require valid price
+        require(_price > 0);
+
+        // increment product-count
+        productCount++;
+
+        // create product (use productCount as product-id)
+        products[productCount] = Product(
+            productCount,
+            _name,
+            _price,
+            msg.sender,
+            false
+        );
+
+        // trigger log event
+        emit ProductCreated(productCount, _name, _price, msg.sender, false);
     }
 }
--- test/Marketplace.test.js
+++ test/Marketplace.test.js
@@ -1,6 +1,8 @@
 const Marketplace = artifacts.require('./Marketplace.sol');
 
-contract('Marketplace', (accounts) => {
+require('chai').use(require('chai-as-promised')).should();
+
+contract('Marketplace', ([deployer, seller, buyer]) => {
     let marketplace;
 
     before(async () => {
@@ -26,5 +28,43 @@ contract('Marketplace', (accounts) => {
             // assert
             assert.equal(name, 'Dapp University Marketplace');
         })
+    })
+
+    describe('deployment', () => {
+        let result, productCount;
+        let productName;
+        let productPrice;
+
+        before(async () => {
+            productName = 'IPhone X';
+            productPrice = web3.utils.toWei('1', 'Ether');
+
+            // using `wei` as currency denomination `'1000000000000000000'`
+            // convert `1` Ether to `Wei` using web3.utils.toWei('1', 'Ether')
+            // pass `seller` in metadata `from` key to let solidity know whose triggers this request
+            // in context, `msg.sender` refers to wallet address passed in the metadata `from` key
+            result = await marketplace.createProduct(productName, productPrice, { 'from': seller });
+            productCount = await marketplace.productCount();
+        });
+
+        it('creates products', async () => {
+            // assert success cases
+            assert.equal(productCount, 1);
+
+            // assert product is created with accurate parameters
+            const event = result.logs[0].args;
+            assert.equal(event.id.toNumber(), productCount.toNumber(), 'id is correct');
+            assert.equal(event.name, productName, 'product name is correct');
+            assert.equal(event.price, '1000000000000000000', 'product price is correct');
+            assert.equal(event.owner, seller, 'product owner is correct');
+            assert.equal(event.purchased, false, 'purchased is correct');
+
+            // assert failure
+            // product must have a name
+            await marketplace.createProduct('', productPrice, { 'from': seller }).should.be.rejected;
+
+            // product must have a price
+            await marketplace.createProduct(productName, 0, { 'from': seller }).should.be.rejected;
+        });
     })
 });

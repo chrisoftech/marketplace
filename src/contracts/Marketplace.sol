2e750a1525e09cc93171d69878525a097db8329f
Christian Onwe <chrisoftech@gmail.com>
2022-02-24 22:38:27 +0000

    Implemented create-product feature usecase.

--- src/contracts/Marketplace.sol
+++ src/contracts/Marketplace.sol
@@ -9,7 +9,7 @@ contract Marketplace {
         uint256 id;
         string name;
         uint256 price;
-        address owner;
+        address payable owner;
         bool purchased;
     }
 
@@ -17,7 +17,15 @@ contract Marketplace {
         uint256 id,
         string name,
         uint256 price,
-        address owner,
+        address payable owner,
+        bool purchased
+    );
+
+    event ProductPurchased(
+        uint256 id,
+        string name,
+        uint256 price,
+        address payable owner,
         bool purchased
     );
 
@@ -46,5 +54,49 @@ contract Marketplace {
 
         // trigger log event
         emit ProductCreated(productCount, _name, _price, msg.sender, false);
+    }
+
+    // annotate all definitions of seller-addres with `payable`
+    // tag to allow value payments to that address
+    function purchaseProduct(uint256 _id) public payable {
+        // fetch product (copy to memory)
+        Product memory _product = products[_id];
+
+        // fetch owner
+        address payable _seller = _product.owner;
+
+        // validate product
+        // require product to have a valid id
+        require(_product.id > 0 && _product.id <= productCount);
+
+        // require payment price to be equal to product price
+        require(msg.value >= _product.price);
+
+        // require that product has not been purchased already
+        require(!_product.purchased);
+
+        // require that seller is not the buyer
+        require(_seller != msg.sender);
+
+        // purchase product (transfer ownership to buyer)
+        _product.owner = msg.sender;
+
+        // mark as purchased
+        _product.purchased = true;
+
+        // update product with new details
+        products[_id] = _product;
+
+        // pay seller by sending them Ether
+        address(_seller).transfer(msg.value);
+
+        // trigger log event
+        emit ProductPurchased(
+            _id,
+            _product.name,
+            _product.price,
+            msg.sender,
+            true
+        );
     }
 }

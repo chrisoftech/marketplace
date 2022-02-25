5afeff3068b7c1728f32c1c63847061190f5adb5
Christian Onwe <chrisoftech@gmail.com>
2022-02-25 16:57:24 +0000

    Refactored and extracted navbar to separate component.

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import Web3 from 'web3';
 import logo from '../logo.png';
 import './App.css';
 import Marketplace from '../abis/Marketplace.json';
+import Navbar from './Navbar';
 
 class App extends Component {
 
@@ -69,21 +70,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Dapp University's Blockchain Marketplace
-          </a>
-          <ul className='navbar-nav px-3'>
-            <li className='nav-item text-nowrap d-none d-sm-none d-sm-block'>
-              <small className='text-white'><span id='account'>{this.state.account}</span></small>
-            </li>
-          </ul>
-        </nav>
+        <Navbar account={this.state.account} />
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
